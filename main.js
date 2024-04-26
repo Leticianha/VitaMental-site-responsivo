3357a869489deec4d36dabfac62390d0c9ff72aa
Leticia Cristina <letici4cr1s@gmail.com>
2024-04-26 11:46:31 -0300

    Initial commitfff

---
+++ main.js
@@ -0,0 +1,13 @@
+function openMenu() {
+    var popup = document.getElementById("popup");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
+
+function closeMenu() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
